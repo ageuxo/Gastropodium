19b938e6e615cca3d5a2b4c9d36d35f5e7252393
ageuxo <ageuxo@gmail.com>
2023-11-27 15:52:54 +0100

    Clean up BlockEdgePath

--- src/main/java/io/github/ageuxo/Gastropodium/entity/pathing/BlockEdgePath.java
+++ src/main/java/io/github/ageuxo/Gastropodium/entity/pathing/BlockEdgePath.java
@@ -2,6 +2,7 @@ package io.github.ageuxo.Gastropodium.entity.pathing;
 
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+import io.github.ageuxo.Gastropodium.mixins.PathAccessor;
 import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.Entity;
@@ -15,56 +16,21 @@ import java.util.List;
 
 @MethodsReturnNonnullByDefault
 public class BlockEdgePath extends Path {
-    private final BlockEdgeNode[] closedSet = new BlockEdgeNode[0];
-    private final BlockEdgeNode[] openSet = new BlockEdgeNode[0];
-    private final BlockEdgeNode target;
-    private final boolean reached;
-    private final List<BlockEdgeNode> edgeNodes;
-    private final float distToTarget;
-    private int nextIndex = 0;
     public static Codec<BlockEdgePath> CODEC = RecordCodecBuilder.create(instance -> instance.group(
             BlockEdgeNode.CODEC.listOf().fieldOf("nodes").forGetter(BlockEdgePath::getEdgeNodes),
             BlockEdgeNode.CODEC.fieldOf("target").forGetter(BlockEdgePath::getTargetNode),
             Codec.BOOL.fieldOf("reached").forGetter(BlockEdgePath::canReach),
             Codec.INT.fieldOf("nextNode").forGetter(BlockEdgePath::getNextNodeIndex)
     ).apply(instance, BlockEdgePath::new));
+    private final BlockEdgeNode target;
 
     public BlockEdgePath(List<BlockEdgeNode> edgeNodes, BlockEdgeNode target, boolean reached, int nextIndex) {
         super(edgeNodes.stream().map(edgeNode -> (Node)edgeNode).toList(), target.asBlockPos(), reached);
-        this.edgeNodes = edgeNodes;
         this.target = target;
-        this.distToTarget = edgeNodes.isEmpty() ? Float.MAX_VALUE : getEdgeNode(edgeNodes.size()-1).distanceTo(target);
-        this.reached = reached;
-        this.nextIndex = nextIndex;
     }
 
     public BlockEdgePath(List<BlockEdgeNode> list, BlockEdgeNode pTargetPos, boolean pReachesTarget) {
         this(list, pTargetPos, pReachesTarget, 0);
-    }
-
-    @Override
-    public void advance() {
-        ++this.nextIndex;
-    }
-
-    @Override
-    public boolean notStarted() {
-        return this.nextIndex <= 0;
-    }
-
-    @Override
-    public boolean isDone() {
-        return this.nextIndex >= this.edgeNodes.size();
-    }
-
-    @Override
-    public int getNextNodeIndex() {
-        return this.nextIndex;
-    }
-
-    @Override
-    public void setNextNodeIndex(int index) {
-        this.nextIndex = index;
     }
 
     @Override
@@ -75,27 +41,12 @@ public class BlockEdgePath extends Path {
         return entityPos.add(offset);
     }
 
-    @Override
-    public boolean canReach() {
-        return this.reached;
-    }
-
     public BlockEdgeNode getEdgeNode(int index){
-        return this.edgeNodes.get(index);
+        return (BlockEdgeNode) ((PathAccessor)this).getNodes().get(index);
     }
 
     public List<BlockEdgeNode> getEdgeNodes(){
-        return this.edgeNodes;
-    }
-
-    @Override
-    public BlockEdgeNode[] getOpenSet() {
-        return openSet;
-    }
-
-    @Override
-    public BlockEdgeNode[] getClosedSet() {
-        return closedSet;
+        return ((PathAccessor)this).getNodes().stream().map(node -> (BlockEdgeNode)node).toList();
     }
 
     @Override
@@ -105,11 +56,6 @@ public class BlockEdgePath extends Path {
 
     public BlockEdgeNode getTargetNode(){
         return this.target;
-    }
-
-    @Override
-    public float getDistToTarget() {
-        return this.distToTarget;
     }
 
     @Override
@@ -130,12 +76,7 @@ public class BlockEdgePath extends Path {
     }
 
     @Override
-    public int getNodeCount() {
-        return this.edgeNodes.size();
-    }
-
-    @Override
     public String toString() {
-        return "BlockEdgePath(length="+this.edgeNodes.size()+")";
+        return "BlockEdgePath(length="+this.getNodeCount()+")";
     }
 }
